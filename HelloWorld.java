69efc3538c4fb0d53f274a07b285b0656149ee3d
EC2 Default User <ec2-user@ip-172-31-21-62.ec2.internal>
2025-04-10 11:28:30 +0000

    Print

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello From Vaishnavi's Jenkins!");
+    System.out.println("Hello From Vaishnavi Jenkins!");
   }
 }
